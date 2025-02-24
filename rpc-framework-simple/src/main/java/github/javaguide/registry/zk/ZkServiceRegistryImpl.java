8080acbea8e13c6879227d8a07dd5fd3f3d0cd65
wuster-xa <3499157213@qq.com>
2025-02-24 14:40:31 +0800

    init commit

---
+++ rpc-framework-simple/src/main/java/github/javaguide/registry/zk/ZkServiceRegistryImpl.java
@@ -0,0 +1,26 @@
+package github.javaguide.registry.zk;
+
+import github.javaguide.registry.ServiceRegistry;
+import github.javaguide.registry.zk.util.CuratorUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+
+import java.net.InetSocketAddress;
+
+/**
+ * service registration  based on zookeeper
+ *
+ * @author shuang.kou
+ * @createTime 2020年05月31日 10:56:00
+ */
+@Slf4j
+public class ZkServiceRegistryImpl implements ServiceRegistry {
+
+    @Override
+    public void registerService(String rpcServiceName, InetSocketAddress inetSocketAddress) {
+        String servicePath = CuratorUtils.ZK_REGISTER_ROOT_PATH + "/" + rpcServiceName + inetSocketAddress.toString();
+        CuratorFramework zkClient = CuratorUtils.getZkClient();
+        //TODO 添加创建临时结点选项，根据SPI动态选择
+        CuratorUtils.createPersistentNode(zkClient, servicePath);
+    }
+}
